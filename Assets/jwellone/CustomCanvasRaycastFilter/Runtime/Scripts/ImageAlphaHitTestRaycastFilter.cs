0394e6e3d68a3e59eaf494ceae99bf5c54852093
YasuhikoUsui <usui_yasuhiko@jwellone.co.jp>
2024-08-18 22:31:07 +0900

    Image.Type.Filled時の処理作成

--- Assets/jwellone/CustomCanvasRaycastFilter/Runtime/Scripts/ImageAlphaHitTestRaycastFilter.cs
+++ Assets/jwellone/CustomCanvasRaycastFilter/Runtime/Scripts/ImageAlphaHitTestRaycastFilter.cs
@@ -26,6 +26,34 @@ namespace jwellone.UI
                 return float.MaxValue;
             }
 
+            var sprite = _image.sprite;
+            switch (_image.type)
+            {
+                case Image.Type.Filled:
+                    {
+                        if (_image.fillMethod == Image.FillMethod.Horizontal || _image.fillMethod == Image.FillMethod.Vertical)
+                        {
+                            var rate = _image.fillMethod == Image.FillMethod.Horizontal
+                                        ? localPoint.x / (sprite.textureRect.size.x * rectTransform.rect.size.x / sprite.rect.size.x) + rectTransform.pivot.x
+                                        : localPoint.y / (sprite.textureRect.size.y * rectTransform.rect.size.y / sprite.rect.size.y) + rectTransform.pivot.y;
+
+                            var min = 0f;
+                            var max = _image.fillAmount;
+                            if (_image.fillOrigin == (int)Image.OriginHorizontal.Right || _image.fillOrigin == (int)Image.OriginVertical.Top)
+                            {
+                                min = 1f - max;
+                                max = 1f;
+                            }
+                            if (min > rate || rate > max)
+                            {
+                                SetDebugRect(rectTransform.rect, Color.white);
+                                return float.MinValue;
+                            }
+                        }
+                    }
+                    break;
+            }
+
             var coord = localPoint / rectTransform.rect.size + rectTransform.pivot;
             var alpha = _image.color.a * texture.GetPixel((int)(coord.x * texture.width), (int)(coord.y * texture.height)).a;
             SetDebugRect(rectTransform.rect, alpha >= alphaHitTestMinimumThreshold ? Color.green : Color.white);
