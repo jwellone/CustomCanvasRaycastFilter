13a4dc88513991d26c5e36f4b69f6a1956e2cf88
YasuhikoUsui <usui_yasuhiko@jwellone.co.jp>
2024-08-28 03:48:25 +0900

    GetAlphaForTypeFilled処理を修正

--- Assets/jwellone/CustomCanvasRaycastFilter/Runtime/Scripts/ImageAlphaHitTestRaycastFilter.cs
+++ Assets/jwellone/CustomCanvasRaycastFilter/Runtime/Scripts/ImageAlphaHitTestRaycastFilter.cs
@@ -63,163 +63,120 @@ namespace jwellone.UI
 
         static float GetAlphaForTypeFilled(in Vector2 localPoint, in Texture2D texture, in Image image)
         {
-            if (0 < image!.fillAmount && image!.fillAmount < 1f)
+            if (0 >= image!.fillAmount || image!.fillAmount >= 1f)
             {
-                var result = false;
-                switch (image.fillMethod)
-                {
-                    case Image.FillMethod.Vertical: result = ValidToFillMethodHorizontalOrVertical(localPoint, image); break;
-                    case Image.FillMethod.Horizontal: result = ValidToFillMethodHorizontalOrVertical(localPoint, image); break;
-                    case Image.FillMethod.Radial90: result = ValidToFillMethodRadial90(localPoint, image); break;
-                    case Image.FillMethod.Radial180: result = ValidToFillMethodRadial180(localPoint, image); break;
-                    case Image.FillMethod.Radial360: result = ValidToFillMethodRadial360(localPoint, image); break;
-                    default: break;
-                }
-
-                if (!result)
-                {
-                    return float.MinValue;
-                }
+                return GetAlphaForTypeSimple(localPoint, texture, image);
             }
 
-            return GetAlphaForTypeSimple(localPoint, texture, image);
-        }
-
-        static bool ValidToFillMethodHorizontalOrVertical(in Vector2 localPoint, in Image image)
-        {
-            var rectTransform = image.rectTransform;
-            var sprite = image.sprite;
-            var rate = image.fillMethod == Image.FillMethod.Horizontal
-                                        ? localPoint.x / (sprite.textureRect.size.x * rectTransform.rect.size.x / sprite.rect.size.x) + rectTransform.pivot.x
-                                        : localPoint.y / (sprite.textureRect.size.y * rectTransform.rect.size.y / sprite.rect.size.y) + rectTransform.pivot.y;
-
-            var min = 0f;
-            var max = image.fillAmount;
-            if (image.fillOrigin == (int)Image.OriginHorizontal.Right || image.fillOrigin == (int)Image.OriginVertical.Top)
-            {
-                min = 1f - max;
-                max = 1f;
-            }
-            if (min > rate || rate > max)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        static bool ValidToFillMethodRadial90(in Vector2 localPoint, in Image image)
-        {
-            var rectTransform = image.rectTransform;
-            var sprite = image.sprite;
-            var fillOrigin = image.fillOrigin;
-            var coord = localPoint / (sprite.textureRect.size * rectTransform.rect.size / sprite.rect.size) + rectTransform.pivot;
-            var rad = 0f;
-
-            if (fillOrigin == (int)Image.Origin90.BottomLeft)
-            {
-                rad = Mathf.Atan2(coord.y, coord.x);
-            }
-            else if (fillOrigin == (int)Image.Origin90.BottomRight)
-            {
-                rad = Mathf.Atan2(1f - coord.x, coord.y);
-            }
-            else if (fillOrigin == (int)Image.Origin90.TopLeft)
-            {
-                rad = Mathf.Atan2(coord.x, 1f - coord.y);
-            }
-            else if (fillOrigin == (int)Image.Origin90.TopRight)
-            {
-                rad = Mathf.Atan2(1f - coord.y, 1f - coord.x);
-            }
-
-            rad /= HALF_PI;
-
-            var min = 0f;
-            var max = image.fillAmount;
-            if (image.fillClockwise)
-            {
-                min = 1f - max;
-                max = 1f;
-            }
-
-            return !(min > rad || rad > max);
-        }
-
-        static bool ValidToFillMethodRadial180(in Vector2 localPoint, in Image image)
-        {
             var rectTransform = image.rectTransform;
             var sprite = image.sprite;
             var fillOrigin = image.fillOrigin;
             var texSize = sprite.textureRect.size * rectTransform.rect.size / sprite.rect.size;
-            var texHalfSize = texSize / 2f;
-            var fillAmount = image!.fillAmount * Mathf.PI;
+            var fillAmount = image!.fillAmount;
             var rad = float.MinValue;
+            var fillClockwise = image!.fillClockwise;
+            switch (image.fillMethod)
+            {
+                case Image.FillMethod.Vertical:
+                    {
+                        rad = localPoint.y / texSize.y + rectTransform.pivot.y;
+                        fillClockwise = fillOrigin == (int)Image.OriginVertical.Top;
+                    }
+                    break;
+                case Image.FillMethod.Horizontal:
+                    {
+                        rad = localPoint.x / texSize.x + rectTransform.pivot.x;
+                        fillClockwise = fillOrigin == (int)Image.OriginHorizontal.Right;
+                    }
+                    break;
+                case Image.FillMethod.Radial90:
+                    {
+                        var coord = localPoint / (sprite.textureRect.size * rectTransform.rect.size / sprite.rect.size) + rectTransform.pivot;
+                        if (fillOrigin == (int)Image.Origin90.BottomLeft)
+                        {
+                            rad = Mathf.Atan2(coord.y, coord.x);
+                        }
+                        else if (fillOrigin == (int)Image.Origin90.BottomRight)
+                        {
+                            rad = Mathf.Atan2(1f - coord.x, coord.y);
+                        }
+                        else if (fillOrigin == (int)Image.Origin90.TopLeft)
+                        {
+                            rad = Mathf.Atan2(coord.x, 1f - coord.y);
+                        }
+                        else if (fillOrigin == (int)Image.Origin90.TopRight)
+                        {
+                            rad = Mathf.Atan2(1f - coord.y, 1f - coord.x);
+                        }
 
-            if (fillOrigin == (int)Image.Origin180.Bottom)
-            {
-                rad = Mathf.Atan2(localPoint.y + texHalfSize.y, -localPoint.x);
-                fillAmount = Mathf.Atan2(Mathf.Sin(fillAmount) * texSize.y, Mathf.Cos(fillAmount) * texHalfSize.x);
-            }
-            else if (fillOrigin == (int)Image.Origin180.Top)
-            {
-                rad = -Mathf.Atan2(localPoint.y - texHalfSize.y, localPoint.x);
-                fillAmount = Mathf.Atan2(Mathf.Sin(fillAmount) * texSize.y, Mathf.Cos(fillAmount) * texHalfSize.x);
-            }
-            else if (fillOrigin == (int)Image.Origin180.Left)
-            {
-                rad = Mathf.Atan2(localPoint.x + texHalfSize.x, localPoint.y);
-                fillAmount = Mathf.Atan2(Mathf.Sin(fillAmount) * texSize.x, Mathf.Cos(fillAmount) * texHalfSize.y);
-            }
-            else if (fillOrigin == (int)Image.Origin180.Right)
-            {
-                rad = -Mathf.Atan2(localPoint.x - texHalfSize.x, -localPoint.y);
-                fillAmount = Mathf.Atan2(Mathf.Sin(fillAmount) * texSize.x, Mathf.Cos(fillAmount) * texHalfSize.y);
-            }
+                        rad /= HALF_PI;
+                    }
+                    break;
 
-            rad /= Mathf.PI;
-            fillAmount /= Mathf.PI;
+                case Image.FillMethod.Radial180:
+                    {
+                        var texHalfSize = texSize / 2f;
+                        fillAmount *= Mathf.PI;
+                        if (fillOrigin == (int)Image.Origin180.Bottom)
+                        {
+                            rad = Mathf.Atan2(localPoint.y + texHalfSize.y, localPoint.x);
+                            fillAmount = Mathf.Atan2(Mathf.Sin(fillAmount) * texSize.y, Mathf.Cos(fillAmount) * texHalfSize.x);
+                        }
+                        else if (fillOrigin == (int)Image.Origin180.Top)
+                        {
+                            rad = -Mathf.Atan2(localPoint.y - texHalfSize.y, -localPoint.x);
+                            fillAmount = Mathf.Atan2(Mathf.Sin(fillAmount) * texSize.y, Mathf.Cos(fillAmount) * texHalfSize.x);
+                        }
+                        else if (fillOrigin == (int)Image.Origin180.Left)
+                        {
+                            rad = Mathf.Atan2(localPoint.x + texHalfSize.x, -localPoint.y);
+                            fillAmount = Mathf.Atan2(Mathf.Sin(fillAmount) * texSize.x, Mathf.Cos(fillAmount) * texHalfSize.y);
+                        }
+                        else if (fillOrigin == (int)Image.Origin180.Right)
+                        {
+                            rad = -Mathf.Atan2(localPoint.x - texHalfSize.x, localPoint.y);
+                            fillAmount = Mathf.Atan2(Mathf.Sin(fillAmount) * texSize.x, Mathf.Cos(fillAmount) * texHalfSize.y);
+                        }
+
+                        rad /= Mathf.PI;
+                        fillAmount /= Mathf.PI;
+                    }
+                    break;
+                case Image.FillMethod.Radial360:
+                    {
+                        var coord = localPoint / texSize + rectTransform.pivot;
+                        var offsets = new[] { -HALF_PI, -PI, HALF_PI, 0f };
+                        rad = Mathf.Atan2(0.5f - coord.y, 0.5f - coord.x) + offsets[image.fillOrigin];
+
+                        while (rad < 0)
+                        {
+                            rad += TWO_PI;
+                        }
+
+                        while (rad > TWO_PI)
+                        {
+                            rad -= TWO_PI;
+                        }
+
+                        rad /= TWO_PI;
+                    }
+                    break;
+            }
 
             var min = 0f;
             var max = fillAmount;
-            if (!image!.fillClockwise)
+            if (fillClockwise)
             {
                 min = 1f - max;
                 max = 1f;
             }
 
-            return !(min > rad || rad > max);
-        }
-
-        static bool ValidToFillMethodRadial360(in Vector2 localPoint, in Image image)
-        {
-            var rectTransform = image.rectTransform;
-            var sprite = image.sprite;
-            var coord = localPoint / (sprite.textureRect.size * rectTransform.rect.size / sprite.rect.size) + rectTransform.pivot;
-            var offsets = new[] { -HALF_PI, -PI, HALF_PI, 0f };
-            var amount = Mathf.Atan2(0.5f - coord.y, 0.5f - coord.x) + offsets[image.fillOrigin];
-
-            while (amount < 0)
+            if (min > rad || rad > max)
             {
-                amount += TWO_PI;
+                return float.MinValue;
             }
 
-            while (amount > TWO_PI)
-            {
-                amount -= TWO_PI;
-            }
-
-            amount /= TWO_PI;
-
-            var min = 0f;
-            var max = image.fillAmount;
-            if (image.fillClockwise)
-            {
-                min = 1f - max;
-                max = 1f;
-            }
-
-            return !(min > amount || amount > max);
+            return GetAlphaForTypeSimple(localPoint, texture, image);
         }
 
 #if UNITY_EDITOR
