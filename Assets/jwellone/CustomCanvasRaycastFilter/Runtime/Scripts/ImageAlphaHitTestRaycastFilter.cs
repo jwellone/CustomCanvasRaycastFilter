fb665b13e8856dddfd633d7b2f6d4b4b1113bc09
YasuhikoUsui <usui_yasuhiko@jwellone.co.jp>
2024-08-28 01:06:41 +0900

    Image.Type別のα取得処理を修正

--- Assets/jwellone/CustomCanvasRaycastFilter/Runtime/Scripts/ImageAlphaHitTestRaycastFilter.cs
+++ Assets/jwellone/CustomCanvasRaycastFilter/Runtime/Scripts/ImageAlphaHitTestRaycastFilter.cs
@@ -11,6 +11,14 @@ namespace jwellone.UI
         const float PI = Mathf.PI;
         const float TWO_PI = PI * 2f;
         const float HALF_PI = PI * 0.5f;
+        delegate float GetAlphaFunc(in Vector2 localPoint, in Texture2D texture, in Image image);
+        readonly static GetAlphaFunc[] _getAlphaFuncs = new GetAlphaFunc[]
+        {
+            GetAlphaForTypeSimple,
+            GetAlphaForTypeSliced,
+            GetAlphaForTypeTiled,
+            GetAlphaForTypeFilled
+        };
 
         Image? _image;
 
@@ -32,51 +40,54 @@ namespace jwellone.UI
             }
 #endif
 
-            if (!ValidType(localPoint))
-            {
-                SetDebugRect(rectTransform.rect, Color.white);
-                return float.MinValue;
-            }
-
-            var coord = localPoint / rectTransform.rect.size + rectTransform.pivot;
-            var alpha = _image.color.a * texture.GetPixel((int)(coord.x * texture.width), (int)(coord.y * texture.height)).a;
+            var alpha = _image.color.a * _getAlphaFuncs[(int)_image.type](localPoint, texture, _image);
             SetDebugRect(rectTransform.rect, alpha >= alphaHitTestMinimumThreshold ? Color.green : Color.white);
             return alpha;
         }
 
-        bool ValidType(in Vector2 localPoint)
+        static float GetAlphaForTypeSimple(in Vector2 localPoint, in Texture2D texture, in Image image)
         {
-            switch (_image!.type)
-            {
-                case Image.Type.Filled:
-                    {
-                        if (_image!.fillAmount >= 1f)
-                        {
-                            return true;
-                        }
-                        else if (_image.fillAmount <= 0f)
-                        {
-                            return false;
-                        }
-
-                        switch (_image.fillMethod)
-                        {
-                            case Image.FillMethod.Vertical: return ValidToFillMethodHorizontalOrVertical(localPoint, _image);
-                            case Image.FillMethod.Horizontal: return ValidToFillMethodHorizontalOrVertical(localPoint, _image);
-                            case Image.FillMethod.Radial90: return ValidToFillMethodRadial90(localPoint, _image);
-                            case Image.FillMethod.Radial180: return ValidToFillMethodRadial180(localPoint, _image);
-                            case Image.FillMethod.Radial360: return ValidToFillMethodRadial360(localPoint, _image);
-                            default: break;
-                        }
-                    }
-                    break;
-            }
-
-            return true;
+            var coord = localPoint / image.rectTransform.rect.size + image.rectTransform.pivot;
+            return texture.GetPixel((int)(coord.x * texture.width), (int)(coord.y * texture.height)).a;
         }
 
-        bool ValidToFillMethodHorizontalOrVertical(in Vector2 localPoint, Image image)
+        static float GetAlphaForTypeSliced(in Vector2 localPoint, in Texture2D texture, in Image image)
         {
+            return GetAlphaForTypeSimple(localPoint, texture, image);
+        }
+
+        static float GetAlphaForTypeTiled(in Vector2 localPoint, in Texture2D texture, in Image image)
+        {
+            return GetAlphaForTypeSimple(localPoint, texture, image);
+        }
+
+        static float GetAlphaForTypeFilled(in Vector2 localPoint, in Texture2D texture, in Image image)
+        {
+            if (0 < image!.fillAmount && image!.fillAmount < 1f)
+            {
+                var result = false;
+                switch (image.fillMethod)
+                {
+                    case Image.FillMethod.Vertical: result = ValidToFillMethodHorizontalOrVertical(localPoint, image); break;
+                    case Image.FillMethod.Horizontal: result = ValidToFillMethodHorizontalOrVertical(localPoint, image); break;
+                    case Image.FillMethod.Radial90: result = ValidToFillMethodRadial90(localPoint, image); break;
+                    case Image.FillMethod.Radial180: result = ValidToFillMethodRadial180(localPoint, image); break;
+                    case Image.FillMethod.Radial360: result = ValidToFillMethodRadial360(localPoint, image); break;
+                    default: break;
+                }
+
+                if (!result)
+                {
+                    return float.MinValue;
+                }
+            }
+
+            return GetAlphaForTypeSimple(localPoint, texture, image);
+        }
+
+        static bool ValidToFillMethodHorizontalOrVertical(in Vector2 localPoint, in Image image)
+        {
+            var rectTransform = image.rectTransform;
             var sprite = image.sprite;
             var rate = image.fillMethod == Image.FillMethod.Horizontal
                                         ? localPoint.x / (sprite.textureRect.size.x * rectTransform.rect.size.x / sprite.rect.size.x) + rectTransform.pivot.x
@@ -97,8 +108,9 @@ namespace jwellone.UI
             return true;
         }
 
-        bool ValidToFillMethodRadial90(in Vector2 localPoint, Image image)
+        static bool ValidToFillMethodRadial90(in Vector2 localPoint, in Image image)
         {
+            var rectTransform = image.rectTransform;
             var sprite = image.sprite;
             var fillOrigin = image.fillOrigin;
             var coord = localPoint / (sprite.textureRect.size * rectTransform.rect.size / sprite.rect.size) + rectTransform.pivot;
@@ -134,13 +146,14 @@ namespace jwellone.UI
             return !(min > rad || rad > max);
         }
 
-        bool ValidToFillMethodRadial180(in Vector2 localPoint, Image image)
+        static bool ValidToFillMethodRadial180(in Vector2 localPoint, in Image image)
         {
+            var rectTransform = image.rectTransform;
             var sprite = image.sprite;
             var fillOrigin = image.fillOrigin;
             var texSize = sprite.textureRect.size * rectTransform.rect.size / sprite.rect.size;
             var texHalfSize = texSize / 2f;
-            var fillAmount = _image!.fillAmount * Mathf.PI;
+            var fillAmount = image!.fillAmount * Mathf.PI;
             var rad = float.MinValue;
 
             if (fillOrigin == (int)Image.Origin180.Bottom)
@@ -174,7 +187,7 @@ namespace jwellone.UI
 
             var min = 0f;
             var max = fillAmount;
-            if (!_image!.fillClockwise)
+            if (!image!.fillClockwise)
             {
                 min = Mathf.PI - max;
                 max = Mathf.PI;
@@ -184,8 +197,9 @@ namespace jwellone.UI
         }
 
 
-        bool ValidToFillMethodRadial360(in Vector2 localPoint, Image image)
+        static bool ValidToFillMethodRadial360(in Vector2 localPoint, in Image image)
         {
+            var rectTransform = image.rectTransform;
             var sprite = image.sprite;
             var coord = localPoint / (sprite.textureRect.size * rectTransform.rect.size / sprite.rect.size) + rectTransform.pivot;
             var offsets = new[] { -HALF_PI, -PI, HALF_PI, 0f };
